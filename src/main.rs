8d053e09a02456823a01b6daaafd45f9fec5cea7
Jeremy D. Frens <jfrens@thecitybase.com>
2019-06-13 15:50:40 -0500

    parse image out of a fractal YAML file (VERY VERY VERY CRUDE)

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,71 @@
 use image;
 use num_complex::Complex;
+use serde_yaml;
+use std::env;
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
-    let x_width = 3.0;
-    let y_height = 2.0;
+    let args: Vec<String> = env::args().collect();
+    let input_filename = &args[1];
+    println!("{:?}", input_filename);
 
-    let left = -0.7 - x_width / 2.0;
-    let top = 0.0 + y_height / 2.0;
+    let mut file = File::open(input_filename).expect("Unable to open file");
+    let mut contents = String::new();
 
-    let width = 768;
-    let height = 512;
+    file.read_to_string(&mut contents)
+        .expect("Unable to read file");
+    let config: serde_yaml::Mapping = serde_yaml::from_str(&contents).expect("unable to parse");
+    let image_config = config.get(&serde_yaml::Value::String("image".to_string()));
+    let image_config = match image_config {
+        Some(ic) => ic,
+        None => panic!("no image!"),
+    };
+    let size = image_config.get(&serde_yaml::Value::String("size".to_string()));
+    let size = match size {
+        Some(s) => s.as_str().unwrap(),
+        None => panic!("no size"),
+    };
+    let size2: Vec<&str> = size.split('x').collect();
+    let size3: Vec<i32> = size2
+        .into_iter()
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect();
+
+    let upper_left = image_config.get(&serde_yaml::Value::String("upperLeft".to_string()));
+    let upper_left = match upper_left {
+        Some(ul) => ul.as_str().unwrap(),
+        None => panic!("no upper left"),
+    };
+    let upper_left2: Vec<&str> = upper_left.split('+').collect();
+    let real: f64 = upper_left2[0].parse().unwrap();
+    let imag_length = upper_left2[1].len();
+    let imag_str = &upper_left2[1].to_string()[0..(imag_length - 1)];
+    let imag: f64 = imag_str.parse().unwrap();
+    let upper_left = Complex::new(real, imag);
+
+    let lower_right = image_config.get(&serde_yaml::Value::String("lowerRight".to_string()));
+    let lower_right = match lower_right {
+        Some(ul) => ul.as_str().unwrap(),
+        None => panic!("no upper left"),
+    };
+    let lower_right2: Vec<&str> = lower_right.split('+').collect();
+    let real: f64 = lower_right2[0].parse().unwrap();
+    let imag_length = lower_right2[1].len();
+    let imag_str = &lower_right2[1].to_string()[0..(imag_length - 1)];
+    let imag: f64 = imag_str.parse().unwrap();
+    let lower_right = Complex::new(real, imag);
+
+    let x_width = (upper_left.re - lower_right.re).abs();
+    let y_height = (lower_right.im - upper_left.im).abs();
+
+    let left = upper_left.re;
+    let top = upper_left.im;
+
+    println!("{:?} {:?} {:?} {:?}", x_width, y_height, left, top);
+
+    let width = size3[0] as u32;
+    let height = size3[1] as u32;
 
     let x_delta = x_width / ((width - 1) as f64);
     let y_delta = y_height / ((height - 1) as f64);
