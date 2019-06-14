f23f0f9d7b7e1ba554eb9c2d4b5b2fb083344d04
Jeremy D. Frens <jfrens@thecitybase.com>
2019-06-14 08:17:40 -0500

    [issue-3] extract fractal iteration and setting a pixel into functions

--- src/main.rs
+++ src/main.rs
@@ -13,22 +13,30 @@ fn main() {
 
     for row in 0..job.image.size.height {
         for col in 0..job.image.size.width {
-            let mut z = Complex::new(0.0, 0.0);
-            let c = job.image.complex_at(col, row);
-            let mut iter = 0;
-
-            while z.norm_sqr() < 4.0 && iter < 512 {
-                z = z * z + c;
-                iter = iter + 1;
-            }
-
+            let iter = iterate(&job.image, col, row);
             let pixel = image.get_pixel_mut(col, row);
-            if iter >= 512 {
-                *pixel = image::Rgb([255, 255, 255]);
-            } else {
-                *pixel = image::Rgb([0, 0, 0]);
-            }
+            set_pixel(pixel, iter);
         }
     }
     image.save("images/fractal.png").unwrap();
+}
+
+fn iterate(image: &fractals::Image, col: u32, row: u32) -> u32 {
+    let mut z = Complex::new(0.0, 0.0);
+    let c = image.complex_at(col, row);
+    let mut iter = 0;
+
+    while z.norm_sqr() < 4.0 && iter < 512 {
+        z = z * z + c;
+        iter = iter + 1;
+    }
+    iter
+}
+
+fn set_pixel(pixel: &mut image::Rgb<u8>, iter: u32) {
+    if iter >= 512 {
+        *pixel = image::Rgb([255, 255, 255]);
+    } else {
+        *pixel = image::Rgb([0, 0, 0]);
+    }
 }
