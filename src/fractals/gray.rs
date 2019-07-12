dd4ce174fe1c5363281a8704cbb61ba2e9a303ca
Jeremy D. Frens <jdfrens@gmail.com>
2019-07-12 18:31:32 -0400

    [issue-19] ColorScheme is now a trait, enum values are structs
    And the struct impl ColorScheme.

--- src/fractals/gray.rs
+++ src/fractals/gray.rs
@@ -3,113 +3,138 @@ use ::image::Rgb;
 use super::color_scheme::ColorScheme;
 use super::escape_time::Iteration;
 
-pub fn gray(color_scheme: &ColorScheme, iter: Iteration) -> Result<Rgb<u8>, ()> {
-  match color_scheme {
-    ColorScheme::BlackOnWhite => match iter {
-      Iteration::Inside { iterations: _ } => Ok(Rgb([0, 0, 0])),
-      Iteration::Outside { iterations: _ } => Ok(Rgb([255, 255, 255])),
-    },
-    ColorScheme::Gray => match iter {
-      Iteration::Inside { iterations: _ } => Ok(Rgb([0, 0, 0])),
-      Iteration::Outside { iterations } => Ok(sqrt_scale(iterations)),
-    },
-    ColorScheme::WhiteOnBlack => match iter {
-      Iteration::Inside { iterations: _ } => Ok(Rgb([255, 255, 255])),
-      Iteration::Outside { iterations: _ } => Ok(Rgb([0, 0, 0])),
-    },
-    &_ => Err(()),
-  }
+#[derive(Debug, PartialEq)]
+pub struct BlackOnWhite {}
+
+impl ColorScheme for BlackOnWhite {
+    fn color(&self, iter: Iteration) -> Rgb<u8> {
+        match iter {
+            Iteration::Inside { .. } => Rgb([0, 0, 0]),
+            Iteration::Outside { .. } => Rgb([255, 255, 255]),
+        }
+    }
 }
 
-fn sqrt_scale(iterations: u32) -> Rgb<u8> {
-  let factor = (iterations as f64 / 512.0).sqrt();
-  let intensity = (factor * 255.0) as u8;
-  Rgb([intensity, intensity, intensity])
+#[derive(Debug, PartialEq)]
+pub struct Gray {}
+
+impl ColorScheme for Gray {
+    fn color(&self, iter: Iteration) -> Rgb<u8> {
+        match iter {
+            Iteration::Inside { .. } => Rgb([0, 0, 0]),
+            Iteration::Outside { iterations } => gray_scale(iterations),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct WhiteOnBlack {}
+
+impl ColorScheme for WhiteOnBlack {
+    fn color(&self, iter: Iteration) -> Rgb<u8> {
+        match iter {
+            Iteration::Inside { .. } => Rgb([255, 255, 255]),
+            Iteration::Outside { .. } => Rgb([0, 0, 0]),
+        }
+    }
+}
+
+fn gray_scale(iterations: u32) -> Rgb<u8> {
+    let factor = (iterations as f64 / 512.0).sqrt();
+    let intensity = (factor * 255.0) as u8;
+    Rgb([intensity, intensity, intensity])
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
-
-  fn inside(iterations: u32) -> Iteration {
-    Iteration::Inside {
-      iterations: iterations,
-    }
-  }
-
-  fn outside(iterations: u32) -> Iteration {
-    Iteration::Outside {
-      iterations: iterations,
-    }
-  }
-
-  mod black_on_white {
-    use super::super::*;
     use super::*;
-    use proptest::prelude::*;
 
-    proptest! {
-      #[test]
-      fn inside_always_black(iterations in 0u32..1024)  {
-        let color = gray(&ColorScheme::BlackOnWhite, outside(iterations));
-        prop_assert_eq!(Ok(Rgb([255, 255, 255])), color);
-      }
-
-      #[test]
-      fn outside_always_white(iterations in 0u32..1024)  {
-        let color = gray(&ColorScheme::BlackOnWhite, inside(iterations));
-            prop_assert_eq!(Ok(Rgb([0, 0, 0])), color);
-      }
-    }
-  }
-
-  mod gray {
-    use super::super::*;
-    use super::*;
-    use ::image::Pixel;
-    use proptest::prelude::*;
-
-    proptest! {
-      #[test]
-      fn inside_always_black(iterations in 0u32..1024)  {
-        let color = gray(&ColorScheme::Gray, inside(iterations));
-        prop_assert_eq!(Ok(Rgb([0, 0, 0])), color);
-      }
+    fn inside(iterations: u32) -> Iteration {
+        Iteration::Inside {
+            iterations: iterations,
+        }
     }
 
-    #[test]
-    fn outside_scaled_gray() {
-      let color_result = gray(&ColorScheme::Gray, outside(128)).unwrap();
-      let channels = color_result.channels();
-      assert_eq!(127, channels[0]);
-      assert_eq!(127, channels[1]);
-      assert_eq!(127, channels[2]);
-
-      let color_result = gray(&ColorScheme::Gray, outside(64)).unwrap();
-      let channels = color_result.channels();
-      assert_eq!(90, channels[0]);
-      assert_eq!(90, channels[1]);
-      assert_eq!(90, channels[2]);
+    fn outside(iterations: u32) -> Iteration {
+        Iteration::Outside {
+            iterations: iterations,
+        }
     }
-  }
 
-  mod white_on_black {
-    use super::super::*;
-    use super::*;
-    use proptest::prelude::*;
+    mod black_on_white {
+        use super::super::*;
+        use super::*;
+        use proptest::prelude::*;
 
-    proptest! {
-      #[test]
-      fn inside_always_white(iterations in 0u32..1024)  {
-        let color = gray(&ColorScheme::WhiteOnBlack, outside(iterations));
-        prop_assert_eq!(Ok(Rgb([0, 0, 0])), color);
-      }
+        proptest! {
+          #[test]
+          fn inside_always_black(iterations in 0u32..1024)  {
+            let cs = BlackOnWhite {};
+            let color = cs.color(outside(iterations));
+            prop_assert_eq!(Rgb([255, 255, 255]), color);
+          }
 
-      #[test]
-      fn outside_always_black(iterations in 0u32..1024)  {
-        let color = gray(&ColorScheme::WhiteOnBlack, inside(iterations));
-            prop_assert_eq!(Ok(Rgb([255, 255, 255])), color);
-      }
+          #[test]
+          fn outside_always_white(iterations in 0u32..1024)  {
+            let cs = BlackOnWhite {};
+            let color = cs.color(inside(iterations));
+                prop_assert_eq!(Rgb([0, 0, 0]), color);
+          }
+        }
     }
-  }
+
+    mod gray {
+        use super::super::*;
+        use super::*;
+        use ::image::Pixel;
+        use proptest::prelude::*;
+
+        proptest! {
+            #[test]
+            fn inside_always_black(iterations in 0u32..1024)  {
+                let cs = Gray {};
+                let color = cs.color(inside(iterations));
+                prop_assert_eq!(Rgb([0, 0, 0]), color);
+            }
+        }
+
+        #[test]
+        fn outside_scaled_gray() {
+            let cs = Gray {};
+
+            let color = cs.color(outside(128));
+            let channels = color.channels();
+            assert_eq!(127, channels[0]);
+            assert_eq!(127, channels[1]);
+            assert_eq!(127, channels[2]);
+
+            let color = cs.color(outside(64));
+            let channels = color.channels();
+            assert_eq!(90, channels[0]);
+            assert_eq!(90, channels[1]);
+            assert_eq!(90, channels[2]);
+        }
+    }
+
+    mod white_on_black {
+        use super::super::*;
+        use super::*;
+        use proptest::prelude::*;
+
+        proptest! {
+            #[test]
+            fn inside_always_white(iterations in 0u32..1024)  {
+                let cs = WhiteOnBlack {};
+                let color = cs.color(inside(iterations));
+                prop_assert_eq!(Rgb([255, 255, 255]), color);
+            }
+
+            #[test]
+            fn outside_always_black(iterations in 0u32..1024)  {
+                let cs = WhiteOnBlack {};
+                let color = cs.color(outside(iterations));
+                prop_assert_eq!(Rgb([0, 0, 0]), color);
+            }
+        }
+    }
 }
--- src/fractals/warp_pov.rs
+++ src/fractals/warp_pov.rs
@@ -3,74 +3,114 @@ use ::image::Rgb;
 use super::color_scheme::ColorScheme;
 use super::escape_time::Iteration;
 
-// Taken from http://warp.povusers.org/Mandelbrot/
-pub fn warp_pov(color_scheme: &ColorScheme, iter: Iteration) -> Result<Rgb<u8>, ()> {
-  let intensities = intensities(iter);
-  match color_scheme {
-    ColorScheme::Blue => Ok(permute_blue(intensities)),
-    ColorScheme::Green => Ok(permute_green(intensities)),
-    ColorScheme::Red => Ok(permute_red(intensities)),
-    &_ => Err(()),
-  }
+// Computations taken from http://warp.povusers.org/Mandelbrot/
+
+#[derive(Debug, PartialEq)]
+pub struct Blue {}
+
+#[derive(Debug, PartialEq)]
+pub struct Green {}
+
+#[derive(Debug, PartialEq)]
+pub struct Red {}
+
+impl ColorScheme for Blue {
+    fn color(&self, iter: Iteration) -> Rgb<u8> {
+        let (primary, secondary) = intensities(iter);
+        Rgb([secondary, secondary, primary])
+    }
 }
 
-fn permute_blue((primary, secondary): (u8, u8)) -> Rgb<u8> {
-  Rgb([secondary, secondary, primary])
+impl ColorScheme for Green {
+    fn color(&self, iter: Iteration) -> Rgb<u8> {
+        let (primary, secondary) = intensities(iter);
+        Rgb([secondary, primary, secondary])
+    }
 }
-fn permute_green((primary, secondary): (u8, u8)) -> Rgb<u8> {
-  Rgb([secondary, primary, secondary])
-}
-fn permute_red((primary, secondary): (u8, u8)) -> Rgb<u8> {
-  Rgb([primary, secondary, secondary])
+
+impl ColorScheme for Red {
+    fn color(&self, iter: Iteration) -> Rgb<u8> {
+        let (primary, secondary) = intensities(iter);
+        Rgb([primary, secondary, secondary])
+    }
 }
 
 fn intensities(iter: Iteration) -> (u8, u8) {
-  match iter {
-    Iteration::Inside { iterations: _ } => (0, 0),
-    Iteration::Outside { iterations } => {
-      let half_iterations = 512 / 2 - 1;
-      if iterations <= half_iterations {
-        (scale(1.max(iterations)), 0)
-      } else {
-        (1 * 255, scale(iterations - half_iterations))
-      }
+    match iter {
+        Iteration::Inside { .. } => (0, 0),
+        Iteration::Outside { iterations } => {
+            let half_iterations = 512 / 2 - 1;
+            if iterations <= half_iterations {
+                (scale(1.max(iterations)), 0)
+            } else {
+                (1 * 255, scale(iterations - half_iterations))
+            }
+        }
     }
-
-  }
 }
 
 fn scale(i: u32) -> u8 {
-  (2.0 * (i - 1) as f64 / 512 as f64 * 255.0) as u8
+    (2.0 * (i - 1) as f64 / 512 as f64 * 255.0) as u8
 }
-
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn test_scale() {
-    assert_eq!(0, scale(1));
-    assert_eq!(125, scale(127));
-    assert_eq!(126, scale(128));
-    // maybe a bit off
-    assert_eq!(253, scale(512));
-  }
+    #[test]
+    fn blue_is_primary() {
+        let cs = Blue {};
+        assert_eq!(
+            Rgb([175, 175, 255]),
+            cs.color(Iteration::Outside { iterations: 432 })
+        );
+    }
 
-  #[test]
-  fn test_intensities() {
-    assert_eq!((0, 0), intensities(Iteration::Outside { iterations: 1 }));
-    assert_eq!(
-      (253, 0),
-      intensities(Iteration::Outside { iterations: 255 })
-    );
-    assert_eq!(
-      (255, 0),
-      intensities(Iteration::Outside { iterations: 256 })
-    );
-    assert_eq!(
-      (255, 255),
-      intensities(Iteration::Outside { iterations: 512 })
-    );
-  }
+    #[test]
+    fn green_is_primary() {
+        let cs = Green {};
+        assert_eq!(
+            Rgb([175, 255, 175]),
+            cs.color(Iteration::Outside { iterations: 432 })
+        );
+    }
+
+    #[test]
+    fn red_is_primary() {
+        let cs = Red {};
+        assert_eq!(
+            Rgb([255, 175, 175]),
+            cs.color(Iteration::Outside { iterations: 432 })
+        );
+    }
+
+    #[test]
+    fn test_scale() {
+        assert_eq!(0, scale(1));
+        assert_eq!(125, scale(127));
+        assert_eq!(126, scale(128));
+        // maybe a bit off
+        assert_eq!(253, scale(512));
+    }
+
+    #[test]
+    fn test_intensities() {
+        assert_eq!((0, 0), intensities(Iteration::Outside { iterations: 1 }));
+        assert_eq!(
+            (253, 0),
+            intensities(Iteration::Outside { iterations: 255 })
+        );
+        assert_eq!(
+            (255, 175),
+            intensities(Iteration::Outside { iterations: 432 })
+        );
+        assert_eq!(
+            (255, 0),
+            intensities(Iteration::Outside { iterations: 256 })
+        );
+        assert_eq!(
+            (255, 255),
+            intensities(Iteration::Outside { iterations: 512 })
+        );
+    }
 }
